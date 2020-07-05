f503cda80e9b2d28d59bf2f0797f6c0314aa4809
Romain Barissat <romain-noreply@barissat.com>
2020-07-05 17:02:17 +0700

    Chore: Rls

--- release.config.js
+++ release.config.js
@@ -4,7 +4,12 @@ console.log(pkg.files)
 
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     [
